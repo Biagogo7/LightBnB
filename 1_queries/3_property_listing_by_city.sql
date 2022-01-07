9b160a4e02acd6a2f06ecbd6166767621a9caf57
Adeyemi <henry.atayero@gmail.com>
2022-01-06 20:37:18 -0600

    updating

--- 1_queries/3_property_listing_by_city.sql
+++ 1_queries/3_property_listing_by_city.sql
@@ -1,10 +1,21 @@
 
+-- SELECT properties.*, avg(property_reviews.rating) as average_rating
+-- FROM properties
+-- JOIN property_reviews ON properties.id = property_id
+-- WHERE city LIKE '%ancouv%'
+-- GROUP BY properties.id
+-- HAVING avg(property_reviews.rating) >= 4
+-- ORDER BY cost_per_night
+-- LIMIT 10;
+
+
+
+
 SELECT properties.*, avg(property_reviews.rating) as average_rating
 FROM properties
 JOIN property_reviews ON properties.id = property_id
-WHERE city LIKE '%ancouv%'
+WHERE city LIKE '%ancouv%' &1
 GROUP BY properties.id
 HAVING avg(property_reviews.rating) >= 4
 ORDER BY cost_per_night
 LIMIT 10;
-
